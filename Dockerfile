c620317a282c2e523cbb6d11f5c8fe4f809830de
willb <willb@linuxacademy.com>
2018-05-18 13:09:16 -0400

    init

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:carbon
+WORKDIR /usr/src/app
+COPY . .
+RUN npm install
+EXPOSE 8080
+CMD [ "npm", "start" ]
---
+++ app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var trainsRouter = require('./routes/trains');
+var metricsRouter = require('./routes/metrics');
+const Prometheus = require('prom-client')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+})
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.locals.startEpoch = Date.now()
+  next()
+})
+
+app.use('/', indexRouter);
+app.use('/trains', trainsRouter);
+app.use('/metrics', metricsRouter);
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+  next(createError(404));
+});*/
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+  next()
+});
+
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now() - res.locals.startEpoch
+  httpRequestDurationMicroseconds
+    .labels(req.method, req.originalUrl, res.statusCode)
+    .observe(responseTimeInMs)
+
+  next()
+})
+
+module.exports = app;
